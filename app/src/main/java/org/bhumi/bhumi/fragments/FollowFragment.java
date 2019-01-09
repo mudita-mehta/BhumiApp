2d605c27927f97e387995249784d3c4854fe60e1
Deepak Chethan <deepakchethan@outlook.com>
2019-01-09 16:26:21 +0530

    Following part fully functional

--- app/src/main/java/org/bhumi/bhumi/fragments/FollowFragment.java
+++ app/src/main/java/org/bhumi/bhumi/fragments/FollowFragment.java
@@ -1,19 +1,27 @@
 package org.bhumi.bhumi.fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import org.bhumi.bhumi.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class FollowFragment extends Fragment {
+public class FollowFragment extends Fragment implements View.OnClickListener {
 
+
+    private String linkedin_link = "https://www.linkedin.com/company/bhumi_2/";
+    private String facebook_link = "https://www.facebook.com/Bhumi.Org/";
+    private String insta_link = "https://www.instagram.com/bhumiorg/";
+    private String twitter_link = "https://twitter.com/BhumiOrg";
+    private String bhumi_link = "https://bhumi.ngo/";
+
+    ImageButton facebookView, twitterView, instagramView, linkedinView;
 
     public FollowFragment() {
         // Required empty public constructor
@@ -24,7 +32,37 @@ public class FollowFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_follow, container, false);
+        View view =  inflater.inflate(R.layout.fragment_follow, container, false);
+        facebookView = view.findViewById(R.id.facebook);
+        twitterView = view.findViewById(R.id.twitter);
+        instagramView = view.findViewById(R.id.instagram);
+        linkedinView = view.findViewById(R.id.linkedin);
+
+        facebookView.setOnClickListener(this);
+        twitterView.setOnClickListener(this);
+        instagramView.setOnClickListener(this);
+        linkedinView.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri websiteToVisit;
+        switch (v.getId()) {
+            case R.id.facebook:
+                websiteToVisit = Uri.parse(facebook_link); break;
+            case R.id.twitter:
+                websiteToVisit = Uri.parse(twitter_link); break;
+            case R.id.instagram:
+                websiteToVisit = Uri.parse(insta_link); break;
+            case R.id.linkedin:
+                websiteToVisit = Uri.parse(linkedin_link); break;
+                default:
+                websiteToVisit = Uri.parse(bhumi_link); break;
+        }
+        intent.setData(websiteToVisit);
+        startActivity(intent);
+    }
 }
