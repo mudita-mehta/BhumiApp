cfe8dddd8981a81c41d24f8de80dd36af192d10c
Sagar C S <sagarstorm@gmail.com>
2019-03-06 12:36:50 +0530

    added firebase and set up notification service

---
+++ app/src/main/java/org/bhumi/bhumi/notifications/NotificationService.java
@@ -0,0 +1,48 @@
+package org.bhumi.bhumi.notifications;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static android.support.constraint.Constraints.TAG;
+
+public  class NotificationService extends FirebaseMessagingService {
+
+    public NotificationService() {
+
+    }
+
+    @Override
+    public  void onMessageReceived(RemoteMessage remoteMessage) {
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+               Log.d(TAG, "received the notification," + " going to handle with long running job");
+            } else {
+                // Handle message within 10 seconds
+                Log.d(TAG, "received the notification, " + " handling with a short running job");
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+}
