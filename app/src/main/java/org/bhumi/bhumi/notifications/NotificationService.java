15028be56ccf498bc1f19fca10c248fe033240b2
Sagar C S <sagarstorm@gmail.com>
2019-03-06 14:42:12 +0530

    added skeleton for FCM messaging

--- app/src/main/java/org/bhumi/bhumi/notifications/NotificationService.java
+++ app/src/main/java/org/bhumi/bhumi/notifications/NotificationService.java
@@ -1,20 +1,28 @@
 package org.bhumi.bhumi.notifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.bhumi.bhumi.R;
+
 import static android.support.constraint.Constraints.TAG;
 
 public  class NotificationService extends FirebaseMessagingService {
 
-    public NotificationService() {
-
-    }
+    public NotificationService() { }
 
     @Override
     public  void onMessageReceived(RemoteMessage remoteMessage) {
@@ -29,7 +37,7 @@ public  class NotificationService extends FirebaseMessagingService {
 
             if (/* Check if data needs to be processed by long running job */ true) {
                 // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-               Log.d(TAG, "received the notification," + " going to handle with long running job");
+                Log.d(TAG, "received the notification," + " going to handle with long running job");
             } else {
                 // Handle message within 10 seconds
                 Log.d(TAG, "received the notification, " + " handling with a short running job");
@@ -45,4 +53,42 @@ public  class NotificationService extends FirebaseMessagingService {
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
     }
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(String messageBody) {
+       //   TODO[developer] : Handle sending notifications here with the messageBody
+    }
+
 }
